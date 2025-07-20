63af391f1ac83c047ffc176aca01089d2e216a9a
Mark <mverleg.noreply@gmail.com>
2025-07-20 10:01:06 +0200

    Add client connection retry

--- src/client.rs
+++ src/client.rs
@@ -4,21 +4,23 @@ use ::clap::Parser;
 use ::env_logger;
 use ::log::debug;
 use ::log::info;
+use std::env::args;
 use ::std::panic;
 use ::std::path::PathBuf;
 use ::std::process::exit;
 use ::std::thread;
+use std::time;
 use ::tonic::transport::Server;
 
-use ::tonic::Response;
-use ::tonic::Status;
+use crate::balancer_svc_client::BalancerSvcClient;
 use futures::StreamExt;
 use log::warn;
 use tokio::io::AsyncWriteExt;
 use tokio::sync::mpsc::channel;
 use tonic::codegen::tokio_stream::wrappers::ReceiverStream;
 use tonic::transport::{Channel, Error, Uri};
-use crate::balancer_svc_client::BalancerSvcClient;
+use ::tonic::Response;
+use ::tonic::Status;
 
 tonic::include_proto!("balancerapi");
 
@@ -30,6 +32,9 @@ pub struct ClientArgs {
     /// The ip and port to connect to
     #[arg(short = 'a', long, default_value = "http://127.0.0.1:7331")]
     pub addr: Uri,
+    /// How many times to retry the initial connection (does not reconnect if disconnected later)
+    #[arg(short = 'r', long, default_value = "1000")]
+    pub max_connection_retry: u32,
 }
 
 #[test]
@@ -44,23 +49,14 @@ async fn main() {
         env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
     );
 
-    run(args.addr).await;
+    run(args.addr, args.max_connection_retry).await;
 }
 
-async fn run(addr: Uri) {
+async fn run(addr: Uri, max_connection_retry: u32) {
     info!("Starting test client, connecting to {addr}");
 
     assert!(addr.scheme().is_some(), "Provide a protocol to -a, like http:// or https://");
-    let mut client = loop {
-        match BalancerSvcClient::connect(addr.clone()).await {
-            Ok(client) => break client,
-            Err(err) => {
-                warn!("Client could not connect to {addr}; err: {err}; will retry");
-                thread::sleep(::std::time::Duration::from_secs(2));
-            }
-        }
-    };
-
+    let mut client = connect_with_retry(&addr, max_connection_retry).await;
     info!("Connected to {addr}");
 
     let (task_sender, task_receiver) = channel::<WorkAcknowledgement>(1);
@@ -76,4 +72,21 @@ async fn run(addr: Uri) {
         }
     }
     info!("End of response stream (server might have stopped, or kicked us)");
+}
+
+async fn connect_with_retry(addr: &Uri, max_connection_retry: u32) -> BalancerSvcClient<Channel> {
+    let mut attempt = 0;
+    loop {
+        attempt += 1;
+        if attempt > max_connection_retry {
+            panic!("Could not connect to {addr} after {max_connection_retry} attempts");
+        }
+        match BalancerSvcClient::connect(addr.clone()).await {
+            Ok(client) => break client,
+            Err(err) => {
+                warn!("Client could not connect to {addr}; err: {err}; retrying ({attempt}/{max_connection_retry})...");
+                thread::sleep(time::Duration::from_secs(2));
+            }
+        }
+    }
 }
